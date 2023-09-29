14a55cf8604ce1010f93a6530a882cca8c90170e
Hugo Ribeiro <hugo.mataribeiro@gmail.com>
2023-09-29 19:02:30 -0300

    chore: setup inicial

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Allu Challenge')
+    .setDescription('Technical Skills Challenge')
+    .setVersion('0.1')
+    .addTag('auth')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
