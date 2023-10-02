54f210e39f5dc80946a3c8a22c8fa08df45de904
Hugo Ribeiro <hugo.mataribeiro@gmail.com>
2023-10-02 10:55:47 -0300

    reafactor: alterando texto da descrição do produto no seeder

--- prisma/seeders/seed.ts
+++ prisma/seeders/seed.ts
@@ -10,7 +10,8 @@ async function seedProducts() {
           name: 'iPhone 13',
           code: 'iphone-13',
           price: 1999.99,
-          description: 'Descrição do iPhone',
+          description:
+            '<p>O iPhone é um smartphone desenvolvido pela <a href="https://www.apple.com/br/" target="_blank">Apple Inc.</a> Ele é conhecido por sua elegância, design sofisticado e tecnologia de ponta. Abaixo estão algumas das características do iPhone:</p><ul><li><strong>Tela:</strong> O iPhone possui uma tela de alta resolução com cores vibrantes.</li><li><strong>Câmera:</strong> Possui uma câmera de alta qualidade para capturar fotos e vídeos incríveis.</li><li><strong>Desempenho:</strong> Equipado com um poderoso processador para executar aplicativos rapidamente.</li><li><strong>Sistema Operacional:</strong> Executa o sistema operacional iOS, conhecido por sua estabilidade e segurança.</li><li><strong>Armazenamento:</strong> Disponível em várias capacidades de armazenamento para atender às necessidades dos usuários.</li></ul><p>Se você procura um smartphone confiável e repleto de recursos, o iPhone é uma excelente escolha.</p><footer><p>Para obter mais informações, visite o <a href="https://www.apple.com/br/iphone/" target="_blank">site oficial do iPhone</a>.</p></footer>',
           image:
             'https://img.freepik.com/fotos-gratis/composicao-elegante-de-smartphone_23-2149437106.jpg?size=626&ext=jpg&ga=GA1.2.761695295.1696214054&semt=sph',
         },
@@ -18,7 +19,8 @@ async function seedProducts() {
           name: 'iPhone 13s ',
           code: 'iphone-13s',
           price: 2299.99,
-          description: 'Descrição do iPhone 13s ',
+          description:
+            '<p>O iPhone é um smartphone desenvolvido pela <a href="https://www.apple.com/br/" target="_blank">Apple Inc.</a> Ele é conhecido por sua elegância, design sofisticado e tecnologia de ponta. Abaixo estão algumas das características do iPhone:</p><ul><li><strong>Tela:</strong> O iPhone possui uma tela de alta resolução com cores vibrantes.</li><li><strong>Câmera:</strong> Possui uma câmera de alta qualidade para capturar fotos e vídeos incríveis.</li><li><strong>Desempenho:</strong> Equipado com um poderoso processador para executar aplicativos rapidamente.</li><li><strong>Sistema Operacional:</strong> Executa o sistema operacional iOS, conhecido por sua estabilidade e segurança.</li><li><strong>Armazenamento:</strong> Disponível em várias capacidades de armazenamento para atender às necessidades dos usuários.</li></ul><p>Se você procura um smartphone confiável e repleto de recursos, o iPhone é uma excelente escolha.</p><footer><p>Para obter mais informações, visite o <a href="https://www.apple.com/br/iphone/" target="_blank">site oficial do iPhone</a>.</p></footer>',
           image:
             'https://img.freepik.com/fotos-gratis/composicao-de-bobinas-criativas_23-2149711507.jpg',
         },
@@ -26,7 +28,8 @@ async function seedProducts() {
           name: 'iPhone 14',
           code: 'iphone-14',
           price: 3039.99,
-          description: 'Descrição do iPhone 14',
+          description:
+            '<p>O iPhone é um smartphone desenvolvido pela <a href="https://www.apple.com/br/" target="_blank">Apple Inc.</a> Ele é conhecido por sua elegância, design sofisticado e tecnologia de ponta. Abaixo estão algumas das características do iPhone:</p><ul><li><strong>Tela:</strong> O iPhone possui uma tela de alta resolução com cores vibrantes.</li><li><strong>Câmera:</strong> Possui uma câmera de alta qualidade para capturar fotos e vídeos incríveis.</li><li><strong>Desempenho:</strong> Equipado com um poderoso processador para executar aplicativos rapidamente.</li><li><strong>Sistema Operacional:</strong> Executa o sistema operacional iOS, conhecido por sua estabilidade e segurança.</li><li><strong>Armazenamento:</strong> Disponível em várias capacidades de armazenamento para atender às necessidades dos usuários.</li></ul><p>Se você procura um smartphone confiável e repleto de recursos, o iPhone é uma excelente escolha.</p><footer><p>Para obter mais informações, visite o <a href="https://www.apple.com/br/iphone/" target="_blank">site oficial do iPhone</a>.</p></footer>',
           image:
             'https://img.freepik.com/fotos-gratis/balanceamento-de-smartphone-com-fundo-rosa_23-2150271746.jpg?size=626&ext=jpg&ga=GA1.2.761695295.1696214054&semt=sph',
         },
@@ -34,7 +37,8 @@ async function seedProducts() {
           name: 'iPhone 14 pro',
           code: 'iphone-14-pro',
           price: 3490.99,
-          description: 'Descrição do iPhone 14 pro',
+          description:
+            '<p>O iPhone é um smartphone desenvolvido pela <a href="https://www.apple.com/br/" target="_blank">Apple Inc.</a> Ele é conhecido por sua elegância, design sofisticado e tecnologia de ponta. Abaixo estão algumas das características do iPhone:</p><ul><li><strong>Tela:</strong> O iPhone possui uma tela de alta resolução com cores vibrantes.</li><li><strong>Câmera:</strong> Possui uma câmera de alta qualidade para capturar fotos e vídeos incríveis.</li><li><strong>Desempenho:</strong> Equipado com um poderoso processador para executar aplicativos rapidamente.</li><li><strong>Sistema Operacional:</strong> Executa o sistema operacional iOS, conhecido por sua estabilidade e segurança.</li><li><strong>Armazenamento:</strong> Disponível em várias capacidades de armazenamento para atender às necessidades dos usuários.</li></ul><p>Se você procura um smartphone confiável e repleto de recursos, o iPhone é uma excelente escolha.</p><footer><p>Para obter mais informações, visite o <a href="https://www.apple.com/br/iphone/" target="_blank">site oficial do iPhone</a>.</p></footer>',
           image:
             'https://img.freepik.com/fotos-gratis/vista-do-balanceamento-de-produtos-eletronicos-no-podio_23-2150141321.jpg?size=626&ext=jpg&ga=GA1.1.761695295.1696214054&semt=sph',
         },
@@ -42,7 +46,8 @@ async function seedProducts() {
           name: 'iPhone 15',
           code: 'iphone-15',
           price: 3959.99,
-          description: 'Descrição do iPhone 15',
+          description:
+            '<p>O iPhone é um smartphone desenvolvido pela <a href="https://www.apple.com/br/" target="_blank">Apple Inc.</a> Ele é conhecido por sua elegância, design sofisticado e tecnologia de ponta. Abaixo estão algumas das características do iPhone:</p><ul><li><strong>Tela:</strong> O iPhone possui uma tela de alta resolução com cores vibrantes.</li><li><strong>Câmera:</strong> Possui uma câmera de alta qualidade para capturar fotos e vídeos incríveis.</li><li><strong>Desempenho:</strong> Equipado com um poderoso processador para executar aplicativos rapidamente.</li><li><strong>Sistema Operacional:</strong> Executa o sistema operacional iOS, conhecido por sua estabilidade e segurança.</li><li><strong>Armazenamento:</strong> Disponível em várias capacidades de armazenamento para atender às necessidades dos usuários.</li></ul><p>Se você procura um smartphone confiável e repleto de recursos, o iPhone é uma excelente escolha.</p><footer><p>Para obter mais informações, visite o <a href="https://www.apple.com/br/iphone/" target="_blank">site oficial do iPhone</a>.</p></footer>',
           image:
             'https://img.freepik.com/fotos-gratis/balanceamento-de-smartphone-com-fundo-violeta_23-2150271744.jpg?size=626&ext=jpg&ga=GA1.1.761695295.1696214054&semt=sph',
         },
